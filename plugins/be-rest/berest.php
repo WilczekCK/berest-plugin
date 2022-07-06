cf9ac11b55876e27a8511ee051a0fb8dfca06502
Paweł Wilk <pwilk0108@gmail.com>
2022-07-06 13:57:40 +0200

    init!

---
+++ plugins/be-rest/berest.php
@@ -0,0 +1,7 @@
+/**
+ * Plugin Name:       BeRest Plugin
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0
+ * Requires PHP:      7.0
+ */
